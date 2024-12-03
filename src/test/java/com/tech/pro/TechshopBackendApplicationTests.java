434a721a1afdf1143bea0595769e23a99eb4d456
Fabio <yfabioyasuo@gmail.com>
2024-12-03 18:37:16 -0300

    removed config class and fixed some files

--- src/test/java/com/tech/pro/TechshopBackendApplicationTests.java
+++ src/test/java/com/tech/pro/TechshopBackendApplicationTests.java
@@ -1,13 +1,7 @@
 package com.tech.pro;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TechshopBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
